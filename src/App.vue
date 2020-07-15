436d1fce37863695d4e274e65d1fdb83f81fa3aa
guimroque <62621598+guimroque@users.noreply.github.com>
2020-07-15 14:45:52 -0300

    pokedex v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/logoPokemon.png" class="is-half is-offset-one-quarter" id="logo">
+      <input type="text" name="" id="" placeholder="Buscar pokemon pelo nome" v-model="busca" class="input is-rounded">
+      <br>
+      <br>
+      <button @click="buscar" class="button is-fullwidth is-link is-rounded"> Buscar </button>
+      <br>
+      <br>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :number="index+1"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: '',
+    } 
+  },
+  created: function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res =>{
+      console.log('Obteve lista de pokemons');
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+    })
+  },
+  methods:{
+    buscar: function(){
+       this.filteredPokemons = this.pokemons;
+       if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+        }
+    }
+  },
+  computed:{
+  
+  },
+  components:{
+    Pokemon
   }
 }
 </script>
@@ -24,5 +61,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo{
+  width: 70%;
 }
 </style>
